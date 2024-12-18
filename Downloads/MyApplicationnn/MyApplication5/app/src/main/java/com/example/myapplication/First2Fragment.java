812006c09d8050156f07e91176c860b86b58d717
syntaxds <148621771+syntaxds@users.noreply.github.com>
2024-12-18 12:25:10 +0700

    Delete Downloads directory

--- Downloads/MyApplicationnn/MyApplication5/app/src/main/java/com/example/myapplication/First2Fragment.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
